f2d5a07b1a4f1c665c4e7151d0ed7fd969ad8efe
Thomas Scholz <info@toscho.de>
2011-02-06 21:42:25 +0100

    class Kraut_Base and a setup function.

--- functions.php
+++ functions.php
@@ -1,1 +1,16 @@
 <?php # -*- coding: utf-8 -*-
+add_action( 'after_setup_theme', array( 'Kraut_Base', 'setup' ) );
+
+class Kraut_Base
+{
+	public static function setup()
+	{
+		! isset ( $GLOBALS['content_width'] ) and $GLOBALS['content_width'] = 600;
+
+		add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
+		add_theme_support( 'automatic-feed-links' );
+		// We support all post formats to ease switching themes.
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'menus' );
+	}
+}
